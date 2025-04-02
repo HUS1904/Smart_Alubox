d47b57d759cf687d56ec5004a240b6dbfdc79103
HUS1904 <144718837+HUS1904@users.noreply.github.com>
2025-04-02 17:01:24 +0200

    First commit

---
+++ Alubox/Alubox/AluboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AluboxApp.swift
+//  Alubox
+//
+//  Created by Hussein Jarrah on 02/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct AluboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
